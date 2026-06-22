22b00e3f3384e80e85d2fd4fe386058f3eb070b0
robsoncabral-program <robsoncabralpp@gmail.com>
2026-06-22 19:14:17 +0100

    Corrigindo a URL de conexão do banco de dados no database.py

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Substitui pela tua password se necessário
-DB_URL = "mysql+mysqlconnector://root:Maranata1978@@localhost/findora_db"
+DB_URL = "mysql+mysqlconnector://root:Maranata1978@localhost/findora_db"
 
 engine = create_engine(DB_URL)
 Session = sessionmaker(bind=engine)
